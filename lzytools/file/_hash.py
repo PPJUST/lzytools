cf99ed8eaac2f484cde9b2aa24708485cd5fed60
PPJUST <106465946+ppjust@users.noreply.github.com>
2025-09-30 10:55:22 +0800

    新增：文件的xxhash算法

--- lzytools/file/_hash.py
+++ lzytools/file/_hash.py
@@ -15,3 +15,13 @@ def calc_xxhash_from_file(file_path: str, block_size: int = 65536) -> str:
         while chunk := f.read(block_size):
             hasher.update(chunk)
     return hasher.hexdigest()
+
+
+def calc_xxhash_from_files(file_paths: list, block_size: int = 65536) -> str:
+    """从文件列表计算xxHash"""
+    hasher = xxhash.xxh64()
+    for file_path in file_paths:
+        with open(file_path, 'rb') as f:
+            while chunk := f.read(block_size):
+                hasher.update(chunk)
+    return hasher.hexdigest()
