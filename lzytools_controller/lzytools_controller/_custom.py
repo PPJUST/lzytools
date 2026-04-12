fd209d28a45ba0906e1cde1d7643e2aeda48cba4
PPJUST <106465946+ppjust@users.noreply.github.com>
2026-04-12 12:28:55 +0800

    新增：更新controller

--- lzytools_controller/lzytools_controller/_custom.py
+++ lzytools_controller/lzytools_controller/_custom.py
@@ -1,12 +1,36 @@
-import os
-
-import filetype
-
+import random
+import time
 
 """----------逻辑函数----------"""
 
 
+def _wait_time(_time: float):
+    """等待时间
+    :param _time:等待时间
+    :return: 等待的时间
+    """
+    if _time == 0:
+        _time = 0.01
+
+    time.sleep(_time)
+
+    return _time
+
+
+def _wait_time_random(min_time: int, max_time: int):
+    """等待随机时间
+    :param min_time: 最小等待时间
+    :param max_time: 最大等待时间
+    :return: 等待的时间
+    """
+    random_time = round(random.uniform(min_time, max_time), 2)
+
+    if random_time == 0:
+        random_time = 0.01
+
+    time.sleep(random_time)
+
+    return random_time
 
 
 """----------调用函数----------"""
-
--- lzytools_controller/lzytools_controller/_keyboard.py
+++ lzytools_controller/lzytools_controller/_keyboard.py
@@ -1,12 +1,83 @@
-import os
+import time
+from typing import Union
 
-import filetype
+import pyautogui
+import pyperclip
 
+from ._pyautogui_keyboard_keys import KEY_NAMES
 
 """----------逻辑函数----------"""
 
 
+def press_chars(chars: str, loop_count: int = 1, loop_interval: float = 0.1):
+    """输入文本
+    :param chars: 要输入的文本
+    :param loop_count: 重复次数
+    :param loop_interval:每次重复的间隔时间（秒）
+    :return: 输入的文本
+    """
+    for _ in range(loop_count):
+        pyperclip.copy(chars)
+        pyautogui.hotkey('ctrl', 'v')
+        time.sleep(loop_interval)
+
+    return chars
+
+
+def click_keys(keys: Union[list, str], loop_count: int = 1, loop_interval: float = 0.1):
+    """逐个敲击指定键，支持多键（不能实现热键组合）
+    :param keys:需要敲击的键
+    :param loop_count: 重复次数
+    :param loop_interval:每次重复的间隔时间（秒）
+    :return: 敲击的键
+    """
+    if type(keys) is str:
+        keys = keys.split(' ')
+
+    for i in keys:
+        if i.lower() not in KEY_NAMES:
+            raise ValueError(f'{i} is not a valid key')
+
+    pyautogui.press(keys=keys, presses=loop_count, interval=loop_interval)
+
+    return keys
+
+
+def press_key(key: str):
+    """按下指定键
+    :param key:要按下的键
+    :return: 按下的键
+    """
+    pyautogui.keyDown(key)
+
+    return key
+
+
+def release_key(key: str):
+    """释放指定键
+    :param key:要释放的键
+    :return: 释放的键
+    """
+    pyautogui.keyUp(key)
+
+    return key
+
+
+def press_hotkey(hotkeys: Union[list, str]):
+    """敲击热键组合
+    :param hotkeys:需要敲击的热键组合
+    :return: 敲击的热键组合
+    """
+    if type(hotkeys) is str:
+        hotkeys = hotkeys.split(' ')
+
+    for i in hotkeys:
+        if i.lower() not in KEY_NAMES:
+            raise ValueError(f'{i} is not a valid key')
+
+    pyautogui.hotkey(hotkeys)
+
+    return hotkeys
 
 
 """----------调用函数----------"""
-
